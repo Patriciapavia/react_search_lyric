e1c5c89916849b97ec0584f9f123e3505f0ced9b
patricia <patriciapavia236@gmail.com>
2020-06-25 13:26:54 +1000

    getting start with seach lyric app

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <span className="navbar-brand mb-0 h1 mx-auto">Search for Lyric</span>
+    </nav>
+  );
+};
+
+export default Navbar;
