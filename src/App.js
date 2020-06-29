117a28127a745d978c88453586386c462c367145
patricia <patriciapavia236@gmail.com>
2020-06-30 09:56:41 +1000

    import provider to app js and install axios to fetch api data

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import { Provider } from "./Context";
 
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Index} />
-          </Switch>
+    <Provider>
+      <Router>
+        <div>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { Consumer } from '../../Context'
+import { Consumer } from "../../Context";
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
-        {value => {
-            
-        }}  
+        {(value) => {
+          console.log(value);
+          return <h1>Tracks</h1>;
+        }}
       </Consumer>
     );
   }
