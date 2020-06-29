5edba945cd67274b44c392795b4b07176d262081
patricia <patriciapavia236@gmail.com>
2020-06-30 09:27:39 +1000

    add componet to search lyric

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Consumer } from '../../Context'
+class Tracks extends Component {
+  render() {
+    return (
+      <Consumer>
+        {value => {
+            
+        }}  
+      </Consumer>
+    );
+  }
+}
+
+export default Tracks;
