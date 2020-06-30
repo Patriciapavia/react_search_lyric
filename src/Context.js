e703676fa31814a8a1697867dc8d578d79db05c2
patricia <patriciapavia236@gmail.com>
2020-06-30 12:23:40 +1000

    add api url and key

--- src/Context.js
+++ src/Context.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: "abc" } },
-      { track: { track_name: "123" } },
-    ],
+    track_list: [],
     heading: "Top 10 Tracks",
   };
+
+  componentDidMount() {
+    const api_key = "be58d4dd8ad4c7c1bc8b65bc552fe4c1";
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1&apikey=${api_key}`
+      )
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }
+
   render() {
     return (
       <div>
